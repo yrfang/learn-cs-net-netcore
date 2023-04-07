6e6c7922bf59a513f585857f6bbbcbd4b9e901b1
Yingru Fang <fyr1116@gmail.com>
2023-04-07 14:15:26 -0700

    Complete NumberGuesser program

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,3 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAppInfo();
+            GreetUser();
+            PlayGame();
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "YR Fang";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+            Console.WriteLine($"Hello {inputName}, let's play a game...");
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void PlayGame()
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Please use an actual number");
+                        continue;
+                    }
+
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+                    }
+                }
+
+                // got the correct number
+                PrintColorMessage(ConsoleColor.Yellow, "CORRECT!! You guessed it!");
+
+                Console.WriteLine("Play again? [Y or N]");
+
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                } else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
